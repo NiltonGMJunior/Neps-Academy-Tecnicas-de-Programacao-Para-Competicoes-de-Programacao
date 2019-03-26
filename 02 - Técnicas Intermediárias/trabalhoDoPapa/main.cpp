8058d682b7bf1f6a0860a08aa2527f22a5237f31
Nilton Gomes Martins Junior <nilton.junior51@hotmail.com>
2019-03-25 21:53:05 -0300

    Update

--- 02 - Técnicas Intermediárias/trabalhoDoPapa/main.cpp
+++ 02 - Técnicas Intermediárias/trabalhoDoPapa/main.cpp
@@ -3,18 +3,47 @@
 
 #define MAX_N 1000
 
+unsigned int cont_recurs = 0; // APENAS PARA BENCHMARK! APAGAR APÓS DEPURAÇÃO!
+
 struct Caixa
 {
-    short int p;
+    long int p;
     long int r;
 };
 
-struct Pilha
+// TODO: Buscar alternativas ao algoritmo implementado na função maior_pilha que rodem em O(f(n)) < O(n!). O algoritmo atualmente implementado aparentemente funcional, mas é muito custoso
+/**
+* Recursão para determinar o maior número possível de caixas que se pode empilhar.
+* A ideia é que, a partir da pilha com a maior resistência possível e uma caixa candidata a ser empilhada.
+* 1 - Verifica se a caixa pode ser empilhada (isto é, se o peso da caixa é menor ou igual à resistência total da pilha);
+* 2 - Desmembra a recursão em duas possibilidades, ou empilha a caixa e verifica com a próxima candidata, ou não empilha a caixa;
+*/
+int maior_pilha(const long int resistencia_pilha, const Caixa *caixas, const unsigned short int indice_caixa, const unsigned short int total_caixas)
 {
-    bool indice_caixas[MAX_N] { false };
-    unsigned short int quantidade_caixas;
-    long int resistencia;
-};
+    std::cout << "Numero de chamadas a recursao: " << ++cont_recurs << "\n"; // APENAS PARA BENCHMARK! APAGAR APÓS DEPURAÇÃO!
+
+	// Caso base da recursão (não há mais caixas a serem empilhadas ou a resistência da pilha está limitada)
+	if (indice_caixa > total_caixas || resistencia_pilha == 0) return 0;
+	// Caso contrário
+	else 
+	{
+		// Verifica se a caixa candidata tem peso maior do que a resistência total da pilha ou não suporta a si mesma
+		if (caixas[indice_caixa].p > resistencia_pilha || caixas[indice_caixa].p > caixas[indice_caixa].r)
+		{
+			// Caso positivo, tenta a próxima caixa e ignora essa
+			return maior_pilha(resistencia_pilha, caixas, indice_caixa + 1, total_caixas);
+		}
+		else
+		{
+			// Caso contrário, define a resistência da pilha como o mínimo entre a resistência da caixa no topo e a resistência da pilha com a caixa no topo
+			long int nova_resistencia = std::min(resistencia_pilha, caixas[indice_caixa].r) - caixas[indice_caixa].p;
+			// Retorna o máximo possível entre empilhar ou não a caixa
+			unsigned short int pilha_com_caixa = 1 + maior_pilha(nova_resistencia, caixas, indice_caixa + 1, total_caixas);
+			unsigned short int pilha_sem_caixa = maior_pilha(resistencia_pilha, caixas, indice_caixa + 1, total_caixas);
+			return std::max(pilha_com_caixa, pilha_sem_caixa);
+		}
+	}
+}
 
 // Retorna a caixa_a se esta possuir uma diferença entre r e p maior do que a caixa_b, e acaixa_b caso contrário
 // Se ambas as caixas possuirem a mesma diferença entre r e p, retorna a caixa com o menor peso
@@ -38,68 +67,19 @@ int main()
     // Ordena as caixas de acordo com a diferença entre sua resistência e peso
     std::sort(caixas, caixas + n, compara_caixas);
 
-    // Declara uma pilha de caixas
-    Pilha pilha;
+    // Declara a quantidade de caixas na pilha
+    unsigned int quantidade_caixas = 0;
 
     // Verifica se a caixa que confere a maior resistência inicial à pilha tem resistência positiva
     if (caixas[0].r - caixas[0].p >= 0)
     {
-        // Empilha a melhor caixa e incrementa a pilha
-        pilha.indice_caixas[0] = true;
-        pilha.quantidade_caixas++;
-        pilha.resistencia = caixas[0].r - caixas[0].p;
-
-        // Enquanto houveram caixas viáveis, verifica qual a melhor caixa a ser empilhada
-        bool ha_caixas_viaveis = true;
-        // Guarda o índice da melhor caixa após cada passada
-        while (ha_caixas_viaveis)
-        {
-            ha_caixas_viaveis = false;
-            // Guarda o índice da melhor caixa à cada iteração
-            short int ind_melhor_caixa = -1;
-            for (unsigned short int iii = 0; iii < n; ++iii)
-            {
-                // Verifica as seguintes condições:
-                // - Se a caixa ainda não foi adicionada na pilha
-                // - Se a resistência da caixa é maior ou igual à seu peso
-                // - Se o peso da caixa é menor ou igual à resistência da pilha
-                if (!pilha.indice_caixas[iii] && caixas[iii]. r >= caixas[iii].p && caixas[iii].p <= pilha.resistencia)
-                {
-                    // Se passou nos testes de validade, verifica se o índice da melhor caixa ainda não foi definido
-                    if (ind_melhor_caixa == -1)
-                    {
-                        // Índice ainda não foi definido, a caixa atual se torna a melhor
-                        ind_melhor_caixa = iii;
-                    }
-                    else
-                    {
-                        // Índice já foi redefinido, procura um candidato melhor
-                        if (std::min(pilha.resistencia - caixas[iii].p, caixas[iii].r - caixas[iii].p) >= std::min(pilha.resistencia - caixas[ind_melhor_caixa].p, caixas[ind_melhor_caixa].r - caixas[ind_melhor_caixa].p))
-                        {
-                            ind_melhor_caixa = iii;
-                        }
-                    }
-                    ha_caixas_viaveis = true;
-                }
-            }
-            // Se alguma caixa viável foi encontrada, adiciona a melhor na pilha
-            if (ha_caixas_viaveis)
-            {
-                pilha.indice_caixas[ind_melhor_caixa] = true;
-                pilha.quantidade_caixas++;
-                pilha.resistencia = std::min(pilha.resistencia - caixas[ind_melhor_caixa].p, caixas[ind_melhor_caixa].r - caixas[ind_melhor_caixa].p);
-            }
-        }
-
+        // Faz a chamada à função recursiva que determina a maior pilha possível
+		quantidade_caixas =  maior_pilha(caixas[0].r - caixas[0].p, caixas, 0, n);
     }
-    //
-    else
-    {
-        pilha.quantidade_caixas = 0;
-    }
+    // Caso contrário, a quantidade permanece zero
 
     // Apresenta a quantidade de caixas na pilha
-    std::cout << pilha.quantidade_caixas << '\n';
+    std::cout << quantidade_caixas << '\n';
 
     return 0;
 }
